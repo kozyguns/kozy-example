e38bc8bf6193e303fb733033d46c8e8bf29e006d
kozyguns <149282419+kozyguns@users.noreply.github.com>
2024-10-02 23:25:25 -0700

    creating a class does not create a stripe product

--- src/utils/stripe/config.ts
+++ src/utils/stripe/config.ts
@@ -1,7 +1,11 @@
 import Stripe from 'stripe';
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? '', {
-  apiVersion: '2024-06-20',
+if (!process.env.STRIPE_SECRET_KEY!) {
+  throw new Error('STRIPE_SECRET_KEY is not set in the environment variables');
+}
+
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: '2024-06-20', // Use the latest available version
   appInfo: {
     name: 'AHR Project',
     version: '0.1.0'
